2bb7b870ebfe1e5763ec8b9e8c09d909dc68861c
YurangJa2 <sang990701@gmail.com>
2020-07-05 22:15:20 +0900

    Updates

---
+++ precache-manifest.f83ad08e0da1b581bb4e90451f9ad6d7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18597462f3f7700ae3a7fe402cdb8f7e",
+    "url": "/trollgg/index.html"
+  },
+  {
+    "revision": "fe344226ea70a877f84b",
+    "url": "/trollgg/static/css/main.d2a3520b.chunk.css"
+  },
+  {
+    "revision": "d9c2992c1440b55cbcb0",
+    "url": "/trollgg/static/js/2.73b94b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trollgg/static/js/2.73b94b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe344226ea70a877f84b",
+    "url": "/trollgg/static/js/main.eacbc611.chunk.js"
+  },
+  {
+    "revision": "40697aceed25161549f7",
+    "url": "/trollgg/static/js/runtime-main.3f981f92.js"
+  },
+  {
+    "revision": "39964f7f7440521c5d53a63cbeed96ff",
+    "url": "/trollgg/static/media/BRONZE.39964f7f.png"
+  },
+  {
+    "revision": "4aaf9754594ca11cbbe6a146ffb0eaa1",
+    "url": "/trollgg/static/media/CHALLENGER.4aaf9754.png"
+  },
+  {
+    "revision": "86ca4007f92cc050b8268f3df4e5bc42",
+    "url": "/trollgg/static/media/DEFAULT.86ca4007.png"
+  },
+  {
+    "revision": "c901832766fb4f856d1f9603208c6761",
+    "url": "/trollgg/static/media/DIAMOND.c9018327.png"
+  },
+  {
+    "revision": "19be519b965d28bf5af7cbb6b7f2471a",
+    "url": "/trollgg/static/media/GOLD.19be519b.png"
+  },
+  {
+    "revision": "d0d4b62f7415cf0ca06d60ce1ae36744",
+    "url": "/trollgg/static/media/GRANDMASTER.d0d4b62f.png"
+  },
+  {
+    "revision": "1a6b4669f3cb62c2edf32971da0b146e",
+    "url": "/trollgg/static/media/IRON.1a6b4669.png"
+  },
+  {
+    "revision": "4156f1c160999db19a69d5bb0e2aaca7",
+    "url": "/trollgg/static/media/MASTER.4156f1c1.png"
+  },
+  {
+    "revision": "86ac5170561afe99a457dccd664eaf72",
+    "url": "/trollgg/static/media/PLATINUM.86ac5170.png"
+  },
+  {
+    "revision": "3a4aa1a5e576ae0ffb79db1f7505ba37",
+    "url": "/trollgg/static/media/SILVER.3a4aa1a5.png"
+  }
+]);
